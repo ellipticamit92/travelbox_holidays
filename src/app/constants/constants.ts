7354bae98aff5cc6ca82f21fd80d18c3c9a3afff
Amit Kumar <ellipticamit@gmail.com>
2025-12-31 16:43:10 +0530

    Add additional phone number constants

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -4,6 +4,9 @@ export const PHOME_NUMBER_HREF = "tel:+917654835326";
 export const PHONE_NUMBER_ONE = "+91 74795 67231";
 export const PHONE_NUMBER_ONE_HREF = "tel:+917479567231";
 
+export const PHONE_NUMBER_TWO = "+91 97987 47463";
+export const PHONE_NUMBER_TWO_HREF = "tel:+919798747463";
+
 export const ADDRESS =
   "Flat no. 201 on 2nd floor in suraj complex,sahayogi press marg, Exhibition Rd, near Domino's pizza, Patna, Bihar 800001";
 
