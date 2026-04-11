9d8e575354ecd362c56a403e9e823ab5cd8b27aa
Amit Kumar <ellipticamit@gmail.com>
2026-04-11 18:17:00 +0530

    dynamic added

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { DestinationsSection } from "@/components/organisms/DestinationsSection";
